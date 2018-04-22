29c17c789ee7d62b43d3ede9d2d95c7dcd46680d
Patryk <sdark431@gmail.com>
2018-04-22 20:43:07 +0200

    Dodanie liczenia czasu

--- Sort.h
+++ Sort.h
@@ -4,11 +4,14 @@
 #include <iostream>
 #include <cstring>
 #include <ctime>
+#include <ratio>
+#include <chrono>
 
 #include "SortSzybkie.h"
 
 
 using namespace std;
+using namespace std::chrono;
 
 //KIERUNEKSORT  1 - rosnaca
 //KIERUNEKSORT -1 - malejaca
@@ -29,28 +32,27 @@ void wypelnijTabllice( T* tWartosci, long wielkoscTablicy ) {
     for (long i = 0 ; i < wielkoscTablicy ; ++i) {
         do
         	id = rand() % wielkoscTablicy;
-        while (*(tWartosci+id));
-        *(tWartosci+id) = i + 1;
+        while (*(tWartosci+i));
+        *(tWartosci+i) = id;
     }
 }
 
 template <typename T>
 grupaSortowania( string rodzaj ,long wielkoscTablicy, T* tWartosciKopia ) {
-    clock_t tStart;
-    clock_t tStop;
-
     cout << rodzaj << "::Sortowanie tablicy " << rodzaj << "[" << wielkoscTablicy << "]" << endl;
     T* tWartosci = new T [wielkoscTablicy];
     wypelnijTabllice(tWartosciKopia, wielkoscTablicy);
 
+    cout << rodzaj << "::Szybkie Sortowanie " << endl;
     memcpy ( tWartosci, tWartosciKopia, wielkoscTablicy*sizeof(T) );
     wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-    cout << rodzaj << "::Szybkie Sortowanie " << endl;
-    tStart = clock();
+
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
     sortSzybkie(tWartosci, 0, wielkoscTablicy-1);
-    tStop = clock();
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
     wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-    cout << "- Czas sortowania " << tStop - tStart << endl;
+    cout << "- Czas sortowania " << time_span.count() << endl;
 
     delete [] tWartosci;
 }
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 //#define WIELKOSCTAB { 10000, 50000, 100000, 500000, 1000000 }
 //unsigned long long WIELKOSCTAB[] = { 10000ull, 50000ull, 100000ull, 500000ull };
-unsigned long long WIELKOSCTAB[] = { 10ull };
+unsigned long long WIELKOSCTAB[] = { 100ull };
 
 int main()
 {
@@ -25,13 +25,13 @@ int main()
         grupaSortowania( "LONG" ,WIELKOSCTAB[i], tLongWartosciKopia);
         delete [] tLongWartosciKopia;
         //-----------------------------------------------------------------
-        float* tFloatWartosciKopia = new float [WIELKOSCTAB[i]];
-        grupaSortowania( "FLOAT" ,WIELKOSCTAB[i], tFloatWartosciKopia);
-        delete [] tFloatWartosciKopia;
-        //-----------------------------------------------------------------
-        double* tDoubleWartosciKopia = new double [WIELKOSCTAB[i]];
-        grupaSortowania( "DOUBLE" ,WIELKOSCTAB[i], tDoubleWartosciKopia);
-        delete [] tDoubleWartosciKopia;
+//        float* tFloatWartosciKopia = new float [WIELKOSCTAB[i]];
+//        grupaSortowania( "FLOAT" ,WIELKOSCTAB[i], tFloatWartosciKopia);
+//        delete [] tFloatWartosciKopia;
+//        //-----------------------------------------------------------------
+//        double* tDoubleWartosciKopia = new double [WIELKOSCTAB[i]];
+//        grupaSortowania( "DOUBLE" ,WIELKOSCTAB[i], tDoubleWartosciKopia);
+//        delete [] tDoubleWartosciKopia;
 
     }
 //      do strukruty dla kazdej grupy
