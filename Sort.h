46174730df106c4741f269e25b669ee1e329c866
Patryk <sdark431@gmail.com>
2018-04-22 23:34:14 +0200

    Dodanie zbierania raportow

--- Sort.h
+++ Sort.h
@@ -6,6 +6,8 @@
 #include <ctime>
 #include <ratio>
 #include <chrono>
+#include <vector>
+#include <memory>
 
 #include "SortSzybkie.h"
 #include "SortScalanie.h"
@@ -15,9 +17,48 @@
 using namespace std;
 using namespace std::chrono;
 
-//KIERUNEKSORT  1 - rosnaca
-//KIERUNEKSORT -1 - malejaca
-#define KIERUNEKSORT 1
+#define WYSW_TAB_SORTSZYB 0
+#define WYSW_TAB_SORTSCAL 0
+#define WYSW_TAB_SORTINTR 0
+
+struct TSortInfo {
+    string nazwaSortowania;
+    long iloscZmiennych;
+    string typZmiennych;
+    double czasSortowania;
+    TSortInfo( string nazwaSortowania, long iloscZmiennych, string typZmiennych, double czasSortowania ):
+        nazwaSortowania(nazwaSortowania),
+        iloscZmiennych(iloscZmiennych),
+        typZmiennych(typZmiennych),
+        czasSortowania(czasSortowania) {}
+    void wyswietl();
+};
+
+void TSortInfo::wyswietl() {
+    cout << "Nazwa: " << nazwaSortowania << ", ";
+    cout << typZmiennych << "[" << iloscZmiennych << "]" << ", ";
+    cout << "Czas: " << czasSortowania << endl;
+}
+
+class CRaport {
+    private:
+       vector< shared_ptr<TSortInfo> > sortInfo;
+    public:
+    CRaport() {}
+    ~CRaport() { sortInfo.clear(); }
+    void dodajSortInfo( shared_ptr<TSortInfo> info ) { sortInfo.push_back(info); }
+    void wyswietl();
+};
+
+void CRaport::wyswietl() {
+    std::cout << "Raport z sortowania" << endl;
+    shared_ptr<TSortInfo> sortowanie;
+    for (vector< shared_ptr<TSortInfo> >::iterator it = sortInfo.begin() ; it != sortInfo.end(); ++it) {
+        sortowanie = *it;
+        sortowanie->wyswietl();
+  }
+  cout << '\n';
+}
 
 template <typename T>
 void wyswietlanieTablicy( T* tWartosci, long wielkoscTablicy ) {
@@ -40,51 +81,49 @@ void wypelnijTabllice( T* tWartosci, long wielkoscTablicy ) {
 }
 
 template <typename T>
-grupaSortowania( string rodzaj ,long wielkoscTablicy, T* tWartosciKopia ) {
+grupaSortowania( string rodzaj ,long wielkoscTablicy, T* tWartosciKopia, CRaport* raport ) {
     high_resolution_clock::time_point t1, t2;
     duration<double> time_span;
-
-    cout << rodzaj << "::Sortowanie tablicy " << rodzaj << "[" << wielkoscTablicy << "]" << endl;
     T* tWartosci = new T [wielkoscTablicy];
     wypelnijTabllice(tWartosciKopia, wielkoscTablicy);
 
     //--->>> Szybkie Sortowanie
-    cout << rodzaj << "::Szybkie Sortowanie " << endl;
     memcpy ( tWartosci, tWartosciKopia, wielkoscTablicy*sizeof(T) );
-    wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-
+    if(WYSW_TAB_SORTSZYB) wyswietlanieTablicy(tWartosci, wielkoscTablicy);
     t1 = high_resolution_clock::now();
     sortSzybkie(tWartosci, 0, wielkoscTablicy-1);
     t2 = high_resolution_clock::now();
     time_span = duration_cast<duration<double>>(t2 - t1);
-    wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-    cout << "- Czas sortowania " << time_span.count() << endl;
+    if(WYSW_TAB_SORTSZYB) wyswietlanieTablicy(tWartosci, wielkoscTablicy);
+    shared_ptr<TSortInfo> shptrSortSzybkie (
+        new TSortInfo("Szybkie", wielkoscTablicy, typeid(T).name(), time_span.count()) );
+    raport->dodajSortInfo(shptrSortSzybkie);
     //---<<<
 
     //--->>> Sortowanie przez scalanie
-    cout << rodzaj << "::Sortowanie przez scalanie " << endl;
     memcpy ( tWartosci, tWartosciKopia, wielkoscTablicy*sizeof(T) );
-    wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-
+    if(WYSW_TAB_SORTSCAL) wyswietlanieTablicy(tWartosci, wielkoscTablicy);
     t1 = high_resolution_clock::now();
     sortScalanie(tWartosci, wielkoscTablicy, 0, wielkoscTablicy-1);
     t2 = high_resolution_clock::now();
     time_span = duration_cast<duration<double>>(t2 - t1);
-    wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-    cout << "- Czas sortowania " << time_span.count() << endl;
+    if(WYSW_TAB_SORTSCAL) wyswietlanieTablicy(tWartosci, wielkoscTablicy);
+    shared_ptr<TSortInfo> shptrSortScalanie (
+        new TSortInfo("Przez scalanie", wielkoscTablicy, typeid(T).name(), time_span.count()) );
+    raport->dodajSortInfo(shptrSortScalanie);
     //---<<<
 
     //--->>> Sortowanie introspetywne
-    cout << rodzaj << "::Sortowanie introspetywne " << endl;
     memcpy ( tWartosci, tWartosciKopia, wielkoscTablicy*sizeof(T) );
-    wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-
+    if(WYSW_TAB_SORTINTR) wyswietlanieTablicy(tWartosci, wielkoscTablicy);
     t1 = high_resolution_clock::now();
     sortIntroSp(tWartosci, wielkoscTablicy);
     t2 = high_resolution_clock::now();
     time_span = duration_cast<duration<double>>(t2 - t1);
-    wyswietlanieTablicy(tWartosci, wielkoscTablicy);
-    cout << "- Czas sortowania " << time_span.count() << endl;
+    if(WYSW_TAB_SORTINTR) wyswietlanieTablicy(tWartosci, wielkoscTablicy);
+    shared_ptr<TSortInfo> shptrSortIntrospec (
+        new TSortInfo("Introspektywne", wielkoscTablicy, typeid(T).name(), time_span.count()) );
+    raport->dodajSortInfo(shptrSortIntrospec);
     //---<<<
 
     delete [] tWartosci;
--- main.cpp
+++ main.cpp
@@ -10,32 +10,33 @@ using namespace std;
 
 //#define WIELKOSCTAB { 10000, 50000, 100000, 500000, 1000000 }
 //unsigned long long WIELKOSCTAB[] = { 10000ull, 50000ull, 100000ull, 500000ull };
-unsigned long long WIELKOSCTAB[] = { 100ull };
+unsigned long long WIELKOSCTAB[] = { 120ull };
 
-int main()
-{
+int main() {
+
+    CRaport raport;
+
     unsigned int wielkosc = sizeof(WIELKOSCTAB)/sizeof(unsigned long long);
     for(int i=0; i<wielkosc; i++) {
         //-----------------------------------------------------------------
         int* tIntWartosciKopia = new int [WIELKOSCTAB[i]];
-        grupaSortowania( "INT" ,WIELKOSCTAB[i], tIntWartosciKopia);
+        grupaSortowania( "INT" ,WIELKOSCTAB[i], tIntWartosciKopia, &raport);
         delete [] tIntWartosciKopia;
         //-----------------------------------------------------------------
         long* tLongWartosciKopia = new long [WIELKOSCTAB[i]];
-        grupaSortowania( "LONG" ,WIELKOSCTAB[i], tLongWartosciKopia);
+        grupaSortowania( "LONG" ,WIELKOSCTAB[i], tLongWartosciKopia, &raport);
         delete [] tLongWartosciKopia;
         //-----------------------------------------------------------------
 //        float* tFloatWartosciKopia = new float [WIELKOSCTAB[i]];
-//        grupaSortowania( "FLOAT" ,WIELKOSCTAB[i], tFloatWartosciKopia);
+//        grupaSortowania( "FLOAT" ,WIELKOSCTAB[i], tFloatWartosciKopia, &raport);
 //        delete [] tFloatWartosciKopia;
 //        //-----------------------------------------------------------------
 //        double* tDoubleWartosciKopia = new double [WIELKOSCTAB[i]];
-//        grupaSortowania( "DOUBLE" ,WIELKOSCTAB[i], tDoubleWartosciKopia);
+//        grupaSortowania( "DOUBLE" ,WIELKOSCTAB[i], tDoubleWartosciKopia, &raport);
 //        delete [] tDoubleWartosciKopia;
 
     }
-//      do strukruty dla kazdej grupy
-//      typ sortowania, typ danych, ilosc danych, upozadkowanie danych
+    raport.wyswietl();
 
     return 0;
 }
